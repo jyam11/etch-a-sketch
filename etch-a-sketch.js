bafe6a03ca543b72741936351c5c7d65cf5b4562
Josh Yam <jyam941@gmail.com>
2021-10-20 15:13:22 -0400

    finished etch a sketch

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,28 +1,49 @@
-const grid = document.querySelector(".grid-container");
-const square = document.querySelector("div");
+const grid = document.querySelector("#grid-container");
+const changeSize = document.querySelector("#grid-size-button");
+const resetGrid = document.querySelector("#resetGrid-button");
+let gridSquare;
 
-function createDiv() {
-    for (i = 0; i < 256; i++) {
-        // Create a new div element
-        const newDiv = document.createElement('div');
-        newDiv.classList.add("square")
-        grid.appendChild(newDiv);
-    }
-};
+function createGrid(size) {
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-square.addEventListener("mouseover", function(event) {
-    event.target.style.backgroundColor = "purple";
-});
-
-function resetGrid() {
-    grid.innerHTML = "";
-
-    let userInput = prompt("how many number of grids would you like?")
-
+    if (size <= 100) {
+        while (grid.firstChild) {
+            grid.removeChild(grid.firstChild);
+        }
+        for (let i = 0; i < size * size; i++) {
+            let div = document.createElement("div");
+            div.classList.add("square");
+            grid.appendChild(div);
+        }
+            gridSquare = document.querySelectorAll(".square");
+            gridSquare.forEach((square) => {
+            square.addEventListener("mouseover", (e) => {
+            square.classList.add("activeGridColor");
+            console.log("mousing over")
+        });
+    });
+    } else {
+    alert("Please select a number between 1 and 100!")
+    createGrid(16);
+}
 
 }
 
+resetGrid.addEventListener('click', () => {
+    gridSquare.forEach((square) => {
+        square.classList.remove("activeGridColor");
+    });
+});
 
-createDiv();
+changeSize.addEventListener("click", () => {
+    gridSquare.forEach((square) => {
+        square.classList.remove("activeGridColor");
+    });
+    size = prompt("How big should the next grid be? Please select 1-100");
+    createGrid(size)
+});
+
+createGrid(16);
 
 
